61344641d5038ac0c05276c643c1604f7d0b8e7d
Edgars <klopojed.son@gmail.com>
2024-02-04 23:50:33 +0200

    Created

---
+++ 005.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 != 0 && i % 5 != 0) {
+			printf("%d\n", i);
+			continue;
+		}
+
+		if (i % 3 == 0) {
+			printf("Fizz");
+		}
+
+		if (i % 5 == 0) {
+			printf("Buzz");
+		}
+
+		putchar('\n');
+	}
+}
