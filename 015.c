4792c9edbf4a8fa3ad3ba48f32fe2c0236db469a
egg4096 <klopojed.son@gmail.com>
2024-02-09 00:54:03 +0200

    New file

---
+++ 015.c
@@ -0,0 +1,18 @@
+// Untested, written on Windows for Linux
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char *line = 0;
+	size_t length;
+	unsigned char i = 0;
+
+	getline(&line, &length, stdin);
+
+	for (; line[i] != 0; i++) {}
+	
+	for (; i > 0; i--) {
+		putchar(line[i]);
+	}
+}
