8d9ef55fe2bf4c4067dab62c478f62700088050d
prodbywerty <72076491+prodbywerty@users.noreply.github.com>
2024-02-12 11:41:36 +0200

    Create 016.c

---
+++ 016.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+    char *line = 0;
+    size_t length;
+    unsigned char vowels = 0;
+    
+    getline(&line, &length, stdin);
+    
+    for (unsigned char i = 0; line[i] != 0; i++) {
+        if (
+            line[i] == 'A' ||
+            line[i] == 'E' ||
+            line[i] == 'I' ||
+            line[i] == 'O' ||
+            line[i] == 'U' ||
+            line[i] == 'Y' ||
+            line[i] == 'a' ||
+            line[i] == 'e' ||
+            line[i] == 'i' ||
+            line[i] == 'o' ||
+            line[i] == 'u' ||
+            line[i] == 'y'
+        ) {
+            vowels += 1;
+        }
+    }
+    
+    printf("%d vowels found.\n", vowels);
+    
+    free(line);
+}
