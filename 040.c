2cdb8a333f8ad21ea3a2f396ea9d27a9f877abc4
prodbywerty <72076491+prodbywerty@users.noreply.github.com>
2024-02-15 12:04:41 +0200

    Update 040.c

--- 040.c
+++ 040.c
@@ -2,15 +2,15 @@
 
 int main(void) {
 	unsigned char array[8] = {93, 39, 18, 98, 16, 45, 98, 26};
-    unsigned char largest = 0;
-    unsigned char largest_index = 0;
+	unsigned char largest = 0;
+	unsigned char largest_index = 0;
 
-    for (unsigned char i = 0; i < 8; i++) {
-        if (array[i] > largest) {
-            largest = array[i];
-            largest_index = i;
-        }
-    }
+	for (unsigned char i = 0; i < 8; i++) {
+		if (array[i] > largest) {
+			largest = array[i];
+			largest_index = i;
+		}
+	}
 
-    printf("%d\n", largest_index);
+	printf("%d\n", largest_index);
 }
